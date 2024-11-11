b842f132bee3cbc24667524c4890b685c47e2c11
WajdButterfly <wajd2828@gmail.com>
2024-11-11 08:09:03 +0300

    Initial Commit

---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Wajd on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
